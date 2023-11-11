7871ab0a0be2e492a718796ae3b90f40bf37478d
MBrock29 <mbrock19@gmail.com>
2023-11-11 13:14:41 -0500

    Solidity version

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -6,7 +6,7 @@ const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.19",
   networks: {
     sepolia: {
       url: ALCHEMY_API_KEY_URL,
