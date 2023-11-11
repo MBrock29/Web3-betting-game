860d75d32550e75ed0980525ac67828abfb0d38f
MBrock29 <mbrock19@gmail.com>
2023-11-11 15:40:35 -0500

    Deleting unused files

--- hardhat/contracts/HelloWorld.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-contract HelloWorld {
-    function sayHello() public pure returns (string memory) {
-        return "Hello, World!";
-    }
-}
